6215cf34321665fd3d554563129d8519235b45de
hgoldstein95 <hgoldstein95@gmail.com>
2014-09-26 23:54:05 -0400

    Added my files: notificator.js test.html

---
+++ notificator.js
@@ -0,0 +1,21 @@
+function changeAllTags(source, tagList) {
+	for(i = 0; i < tagList.length; i++) {
+		source = changeTags(source, tagList[i]);
+	}
+	return source;
+}
+
+function changeTags(source, tag) {
+	source = source.replace("\\" + tag, "<!--" + tag + "-->");
+	source = source.replace("\\" + tag + "*", "<!--" + tag + "*-->");
+	return source
+}
+
+function filterTags(source, tag) {
+	var filteredText = "";
+	while(source.indexOf("\\" + tag) != -1 && source.indexOf("\\" + tag + "*") != -1) {
+		filteredText += source.substring(source.indexOf("\\" + tag) + (tag.length + 1), source.indexOf("\\" + tag + "*"));
+		source = source.substring(source.indexOf("\\" + tag + "*") + (tag.length + 2), source.length);
+	}
+	return filteredText;
+}
