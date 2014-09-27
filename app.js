96c737be626492e0d83a0dd62a4699cb07723658
hgoldstein95 <hgoldstein95@gmail.com>
2014-09-27 01:38:51 -0400

    Added lots of files for node server and stuff.

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+app.get('/', function(req, res) {
+	res.send('hello world');
+});
+app.use(express.static(__dirname));
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
