78c1d86f5168d8a3fbd70cd3073a566bd17d5b8e
Naman Agarwal <na298@cornell.edu>
2014-09-27 03:40:35 -0400

    things are uploading files to me all the files are belong to me

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 var express = require('express');
+var busboy = require('connect-busboy');
+var fs = require('fs');
 var app = express();
+app.use(busboy());
 app.get('/', function(req, res) {
 	res.send('hello world');
+});
+app.post('/img', function(req, res) {
+	var fstream;
+	req.pipe(req.busboy);
+	req.busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+		console.log('got a file');
+		fstream = fs.createWriteStream('files/' + filename);
+		file.pipe(fstream);
+		fstream.on('close', function() {
+			console.log('finished uploading file');
+		});
+	});
+	req.busboy.on('finish', function() {
+		console.log('finished request');
+		res.send('something something image');
+	});
 });
 app.use(express.static(__dirname));
 var server = app.listen(3000, function() {
